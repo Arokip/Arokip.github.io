a9cb1256d82af788a56a29218a19e9ea6ffc5a5d
unknown <arokip.quique@gmail.com>
2021-04-04 00:38:56 +0200

    add urls

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyPage());
 }
 
 class MyPage extends StatelessWidget {
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) {
-          return Scaffold(
-            body: SafeArea(
-              child: Container(
-                color: Colors.amber,
-                child: Center(
-                  child: Column(
-                    children: [
-                      Text('my-gh-page'),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/fdl');
-                        },
-                        child: Text('fdl'),
-                      )
-                    ],
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(
+            color: Colors.amber,
+            child: Center(
+              child: Column(
+                children: [
+                  Text('My personal page.'),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    child: Text('github link to fdl'),
+                    onPressed: () {
+                      _launchURL('https://github.com/Arokip/fdl');
+                    },
                   ),
-                ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    child: Text('fdl demo app'),
+                    onPressed: () {
+                      _launchURL('https://arokip.github.io/fdl_demo_app');
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    child: Text('fdl old'),
+                    onPressed: () {
+                      _launchURL('https://arokip.github.io/fdl_old');
+                    },
+                  ),
+                ],
               ),
             ),
-          );
-        },
-      },
+          ),
+        ),
+      ),
     );
   }
 }
