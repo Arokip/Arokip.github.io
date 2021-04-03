b2fccc6d919b3945725f7cf3fa58d2cbb9578944
unknown <arokip.quique@gmail.com>
2021-04-04 00:17:03 +0200

    my page

--- android/app/src/main/java/cz/arokip/my_gh_page/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package cz.arokip.my_gh_page;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- android/app/src/main/kotlin/cz/arokip/my_gh_page/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package cz.arokip.my_gh_page
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
