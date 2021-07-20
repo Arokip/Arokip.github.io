d4887beb93d701e4045f6ac12ef04285f854b691
Jindra <jindrich.pikora@gmail.com>
2021-07-20 14:05:30 +0200

    add link to etl; change color

--- lib/main.dart
+++ lib/main.dart
@@ -20,23 +20,31 @@ class MyPage extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Container(
-            color: Colors.amber,
+            color: Colors.amber[100],
             child: Center(
               child: Column(
                 children: [
+                  SizedBox(height: 40),
                   Text('My personal page.'),
                   SizedBox(height: 16),
                   ElevatedButton(
-                    child: Text('github link to fdl'),
+                    child: Text('github link to diagram_editor library'),
                     onPressed: () {
                       _launchURL('https://github.com/Arokip/fdl');
                     },
                   ),
                   SizedBox(height: 16),
                   ElevatedButton(
-                    child: Text('fdl demo app'),
+                    child: Text('library demo apps'),
                     onPressed: () {
                       _launchURL('https://arokip.github.io/fdl_demo_app');
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  ElevatedButton(
+                    child: Text('ETL'),
+                    onPressed: () {
+                      _launchURL('https://arokip.github.io/etl_diagram_editor');
                     },
                   ),
                   SizedBox(height: 16),
